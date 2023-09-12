e10f693d540226e64a5556b0960cb3d088287fb4
Atharva <atharvakn@gmail.com>
2023-09-12 17:14:30 +0530

    refac: data types for cache functions

--- src/service/gasEstimate.ts
+++ src/service/gasEstimate.ts
@@ -1,19 +1,21 @@
 export interface GasEstimate {
   contractAddress: string
   functionSignature: string
-  gasUsed: number
-  gasRefund: number
+  gasUsed: string
+  gasRefund: string
   timestamp: number
 }
 
-export function checkEntry(entry: GasEstimate): boolean {
+export function checkEntry(contractAddress: string, functionSig: string): boolean {
   return true
 }
 
 export function addEntry(entry: GasEstimate): void {
   // placeholder
+  // Add new entry
+  // If present, always overwrite
 }
 
-export function getGasEstimate(): void {
-  // placeholder
+export function getGasEstimate(contractAddress: string, functionSig: string): GasEstimate {
+  throw new Error('Function not implemented')
 }
