2fac961ee7bba06ce30c233a3741599af613a4e8
kgmyatthu <2962000kaungmyatthu@gmail.com>
2024-04-18 14:37:13 +0700

    feat: newHead subscription support

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -370,7 +370,7 @@ export const setupSubscriptionEventHandlers = (ipport: string): void => {
     }
   })
 
-  subscriptionEventEmitter.on('evm_newblock_received', (newblock) => {
+  subscriptionEventEmitter.on('evm_newHead_received', (newblock) => {
     try {
       for (let [key, value] of blockSubscriptionList) {
         if (value.socket.readyState === 2 || value.socket.readyState === 3) {
