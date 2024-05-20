13ce31d225bbbaab65cdc208ecc92a4191f57b10
jairajdev <jairajranamagar.dev@gmail.com>
2024-05-20 22:57:53 +0545

    Correct tx receipt data

--- src/external/Collector.ts
+++ src/external/Collector.ts
@@ -468,7 +468,7 @@ class Collector extends BaseExternal {
         from: readableReceipt.from,
         gas: readableReceipt.gasUsed,
         value: readableReceipt.value,
-        input: readableReceipt.input,
+        input: readableReceipt.data,
         gasPrice: readableReceipt.gasPrice,
         chainId: '0x' + CONFIG.chainId.toString(16),
         transactionIndex: readableReceipt.transactionIndex,
