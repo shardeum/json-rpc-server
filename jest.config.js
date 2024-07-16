f289260a6ac9f070b916126fb883962b3abb68d8
paulgs9988 <paulgs9988@gmail.com>
2024-07-16 16:34:39 -0600

    updating configs for coverage report generation

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,19 @@ module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.ts'],
     moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1'
+      '^@/(.*)$': '<rootDir>/src/$1'
     },
     testTimeout: 20000,
-};
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "/dist/",
+      "/src/server.js" 
+    ],
+    transform: {
+      "^.+\\.(ts|tsx)$": "ts-jest",
+    },
+  };
+  
