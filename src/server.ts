1e0840a9300699b466c4db93505c4523ade1f98e
Tanuj Soni <tanuj@shardeum.org>
2024-10-08 16:49:01 +0530

    Refactor requestLogger middleware

--- src/server.ts
+++ src/server.ts
@@ -88,9 +88,7 @@ app.set('trust proxy', config.trustProxy)
 app.use(cors({ methods: ['POST'] }))
 app.use(express.json())
 app.use(cookieParser())
-if (config.enableRequestLogger) {
-  app.use(requestLogger)
-}
+app.use(requestLogger)
 app.use(function (req, res, next) {
   res.setHeader('X-Content-Type-Options', 'nosniff')
   res.setHeader(
