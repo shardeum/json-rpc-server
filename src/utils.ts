aba70af9ae65b59034c87ced1253a7478a2a0293
Ekene Eze <ekeneeze3@gmail.com>
2025-01-21 18:14:28 +0400

    OPEN-26: Expose Network Information via the JSON-RPC interface (#82)
    * add nodelist and network account methods
    
    * add cycleinfo method
    
    * update retry mechanism
    
    * use locked dep & increase cache ttl
    
    * Regenerate package-lock.json
    
    * fix lint error
    
    * fix format check
    
    * update patch file
    
    * update config
    
    * update patch
    
    * use network slashing values
    
    * removed the added library cache-memory and used the custom implemented ttl map instead
    
    * removed the redundant comments
    
    * removed one more redundant comment
    
    ---------
    
    Co-authored-by: devendra-shardeum <devendra@shardeum.org>

--- src/utils.ts
+++ src/utils.ts
@@ -33,6 +33,7 @@ import {
 import Sntp from '@hapi/sntp'
 import { randomBytes, createHash } from 'crypto'
 import net from 'net'
+import { TTLMap } from './utils/TTLMap'
 
 crypto.init('69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc')
 
@@ -44,6 +45,14 @@ export const node = {
   ip: '127.0.0.1',
   port: 9001,
 }
+
+
+const NODE_LIST_CACHE_TTL = 60 * 1000 // Cache snapshot for 1 minute
+const nodeListSnapshotCache = new TTLMap<Node[]>() // New cache for snapshots
+
+
+const NETWORK_ACCOUNT_CACHE_TTL = 30 * 1000 // Convert 30 seconds to milliseconds
+const networkAccountSnapshotCache = new TTLMap<any>() // Snapshot Cache
 
 let rotationEdgeToAvoid = 0
 
@@ -114,7 +123,7 @@ export async function updateNodeList(tryInfinate = false): Promise<void> {
   console.log(`Updating NodeList from ${getArchiverUrl().url}`)
 
   console.time('nodelist_update')
-  const nRetry = tryInfinate ? -1 : 0 // infinitely retry or no retries
+  const nRetry = tryInfinate ? -1 : 5 // infinitely retry or 5 retries if initial request fails
   if (config.askLocalHostForArchiver === true) {
     if (gotArchiver === false) {
       gotArchiver = true
@@ -184,7 +193,57 @@ export async function updateNodeList(tryInfinate = false): Promise<void> {
     }
   }
   console.timeEnd('nodelist_update')
+  nodeListSnapshotCache.set("snapshot", [...nodeList], NODE_LIST_CACHE_TTL)
+  
 }
+
+export async function getNodeList(page: number, limit: number): Promise<any> {
+  try {
+    
+    let nodeListCache = await nodeListSnapshotCache.get("nodeListCache")
+
+    // If no snapshot is available, take a fresh one
+    if (!nodeListCache) {
+      nodeListCache = [...nodeList]
+      nodeListSnapshotCache.set("nodeListCache", nodeListCache, NODE_LIST_CACHE_TTL)
+    }
+
+    const startIndex = (page - 1) * limit
+    const endIndex = startIndex + limit
+    const paginatedNodeList = nodeListCache.slice(startIndex, endIndex)
+
+    return {
+      nodes: paginatedNodeList,
+      totalNodes: nodeListCache.length,
+      page: page,
+      limit: limit,
+      totalPages: Math.ceil(nodeListCache.length / limit),
+    }
+  } catch (error) {
+    console.error('Error in getNodeList:', error)
+    throw error
+  }
+}
+
+
+export async function getNetworkAccount(): Promise<any> {
+  try {
+    
+    let cachedAccount = await networkAccountSnapshotCache.get("networkAccountCache")
+
+    if (!cachedAccount) {
+      const response = await axios.get(`${getArchiverUrl().url}/get-network-account?hash=false`)
+      cachedAccount = response.data
+      networkAccountSnapshotCache.set("networkAccountCache", cachedAccount, NETWORK_ACCOUNT_CACHE_TTL)
+    }
+
+    return cachedAccount
+  } catch (error) {
+    console.error('Error fetching network account:', error)
+    throw error
+  }
+}
+
 
 export function removeFromNodeList(ip: string, port: string): void {
   nodeList = nodeList.filter((node) => node.ip !== ip || node.port !== Number(port))
@@ -955,8 +1014,14 @@ export class RequestersList {
     const heavyReqHistory = this.heavyRequests.get(ip)
 
     if (heavyReqHistory && heavyReqHistory.length >= config.rateLimitOption.allowedHeavyRequestPerMin + 1) {
-      if (now - heavyReqHistory[heavyReqHistory.length - config.rateLimitOption.allowedHeavyRequestPerMin] < oneMinute) {
-        if (verbose) console.log(`Ban this ip ${ip} due to continuously sending more than ${config.rateLimitOption.allowedHeavyRequestPerMin} reqs in 60s`)
+      if (
+        now - heavyReqHistory[heavyReqHistory.length - config.rateLimitOption.allowedHeavyRequestPerMin] <
+        oneMinute
+      ) {
+        if (verbose)
+          console.log(
+            `Ban this ip ${ip} due to continuously sending more than ${config.rateLimitOption.allowedHeavyRequestPerMin} reqs in 60s`
+          )
         this.addToBlacklist(ip)
         if (config.recordTxStatus && reqType === 'eth_sendRawTransaction') {
           const transaction = getTransactionObj({ raw: reqParams[0] })
