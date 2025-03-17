9de27227d8afd087a0eafafa75c25ddf1c7fbfc0
Sam Sweet <samwsweet@gmail.com>
2025-03-17 14:39:40 -0400

    remove unused check

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -393,7 +393,6 @@ export const setupSubscriptionEventHandlers = (ipport: string): void => {
   subscriptionEventEmitter.on('evm_newHead_received', (newblock) => {
     try {
       for (let [key, value] of blockSubscriptionList) {
-        if (value.socket.readyState === 2 || value.socket.readyState === 3) {
         if (value.socket.readyState === WebSocket.CLOSED || value.socket.readyState === WebSocket.CLOSING) {
           console.log('[NewHeads] Client socket closed, cleaning up subscription:', key)
           subscriptionEventEmitter.emit('evm_newHead_unsubscribe', key)
